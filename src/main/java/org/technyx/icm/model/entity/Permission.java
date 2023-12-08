ba56c616626ca5acf6ea091aab6948ca752e9e91
fatemealimardani <f.alimardni@gmail.com>
2023-12-08 14:27:30 +0330

    entity: file paySlip permission added

---
+++ src/main/java/org/technyx/icm/model/entity/Permission.java
@@ -0,0 +1,29 @@
+package org.technyx.icm.model.entity;//package org.technyx.icm.model.entity;
+//
+//import com.google.gson.Gson;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.experimental.SuperBuilder;
+//
+//@Getter
+//@Setter
+//@SuperBuilder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity(name = "Permission")
+//@Table(name = "tb_permission")
+//public class Permission extends BaseObject{
+//
+//    @Column(name = "c_title")
+//    private String title;
+//
+//    @Override
+//    public String toString() {
+//        return new Gson().toJson(this);
+//    }
+//}
