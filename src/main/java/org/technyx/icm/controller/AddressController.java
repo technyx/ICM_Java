c2b4f99a284cd2641a3172ab8aaa61652c78cebe
Ali Mirzad <97289679+AliMirzad@users.noreply.github.com>
2023-12-10 15:40:10 +0330

    address controller addded

--- src/main/java/org/technyx/icm/controller/AddressController.java
+++ src/main/java/org/technyx/icm/controller/AddressController.java
@@ -1,7 +1,6 @@
 package org.technyx.icm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.technyx.icm.model.dtos.AddressDto;
@@ -17,67 +16,26 @@ public class AddressController {
     private AddressService service;
 
     @PostMapping
-    public ResponseEntity<String> save(@RequestBody AddressDto dto) {
-        ResponseEntity<String> response = null;
-        try {
-            AddressDto savedDto = service.save(dto);
-            if (savedDto != null) {
-                response = ResponseEntity
-                        .status(HttpStatus.CREATED)
-                        .body("given extra-info has been created");
-            }
-        } catch (Exception ex) {
-            response = ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("an exception occurred due to " + ex.getMessage());
-        }
-        return response;
+    public ResponseEntity<AddressDto> save(@RequestBody AddressDto dto) {
+        return ResponseEntity.ok().body(service.save(dto));
     }
 
-//    @PutMapping
-//    public ResponseEntity<String> update(@RequestBody AddressDto dto) {
-//        ResponseEntity<String> response = null;
-//        try {
-//            AddressDto updatedDto = service.update(dto);
-//            if (updatedDto != null) {
-//                response = ResponseEntity
-//                        .status(HttpStatus.CREATED)
-//                        .body("given extra-info has been updated");
-//            }
-//        } catch (Exception ex) {
-//            response = ResponseEntity
-//                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body("an exception occurred due to " + ex.getMessage());
-//        }
-//        return response;
-//    }
-//
-//    @DeleteMapping
-//    public ResponseEntity<String> delete(@RequestBody AddressDto dto) {
-//        ResponseEntity<String> response = null;
-//        try {
-//            service.delete(dto);
-//            response = ResponseEntity
-//                    .status(HttpStatus.CREATED)
-//                    .body("given extra-info has been deleted");
-//
-//        } catch (Exception ex) {
-//            response = ResponseEntity
-//                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body("an exception occurred due to " + ex.getMessage());
-//        }
-//        return response;
-//    }
-//
-//    /*
-//     * TODO: remember to implement showSingle
-//     * */
-//
-//    @GetMapping
-//    public ResponseEntity<List<AddressDto>> showAll() {
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .body(service.showList());
-//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressDto> update(@PathVariable long id, @RequestBody AddressDto dto) {
+        return ResponseEntity.ok().body(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressDto>> showAll() {
+        return ResponseEntity
+                .ok()
+                .body(service.showList());
+    }
 
 }
