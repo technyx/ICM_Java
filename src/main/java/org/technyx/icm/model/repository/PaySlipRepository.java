08baea6d2527f7f0fc613ae7844ebab9ee5df44f
AliMirzad <immirzad@gmail.com>
2024-01-07 01:59:05 +0330

    payslip new api

--- src/main/java/org/technyx/icm/model/repository/PaySlipRepository.java
+++ src/main/java/org/technyx/icm/model/repository/PaySlipRepository.java
@@ -2,6 +2,7 @@ package org.technyx.icm.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.technyx.icm.model.entity.PaySlip;
 
@@ -13,20 +14,20 @@ public interface PaySlipRepository extends JpaRepository<PaySlip, Long> {
     @Query(
             nativeQuery = true,
             value = """
-                    select * from tb_pay_slip where fk_user = ?1
+                    select * from tb_pay_slip where fk_user = :userId
                     """
     )
-    List<PaySlip> findAllByUserId(long userId);
+    List<PaySlip> findAllByUserId(@Param("userId") long userId);
 
     @Query(
             nativeQuery = true,
             value = """
-                    select * from tb_pay_slip as ps
+                    select ps.* from tb_pay_slip as ps
                     join tb_user as us on us.c_id = ps.fk_user
-                    where us.c_username = ?1
+                    where us.c_username = :username
                     """
     )
-    List<PaySlip> findAllByUserUsername(String username);
+    List<PaySlip> findAllByUserUsername(@Param("username") String username);
 
 
     @Query(
